fe01f9c9dcee207ca2b0554786c0f516ff0428e7
Mitchell <m.a.j.w.neijenhuizen>
2020-05-01 14:44:07 +0200

    Updates1

--- src/app/calendar-two/calendar-two.component.ts
+++ src/app/calendar-two/calendar-two.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listWeekPlugin from '@fullcalendar/list';
+import listDayPlugin from '@fullcalendar/list';
+import { EventInput } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction'
 
 @Component({
   selector: 'app-calendar-two',
@@ -8,11 +12,39 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class CalendarTwoComponent implements OnInit {
 
+  calendarPlugins = [dayGridPlugin, listWeekPlugin, listDayPlugin, interactionPlugin];
+  calendarEvents: EventInput[] = [
+    { title : 'weekend', start: new Date ( 'May 1, 2020, 16:30:00')  } 
+  ]
+
   constructor() { }
+  dayRender(args) {
+    // console.log(args) 
+    console.log("Double click"); //Console output 
+    var cell:HTMLElement = args.el; 
+    cell.ondblclick = (ev: MouseEvent) => {
+      this.addEvent(args.date);
+
+    }
+  }
+
+
+  addEvent(date) {
+    console.log("I am doing something"); //Console output 
+    console.log(this.calendarEvents)
+    var title = prompt('Enter event title');
+    if (title == '')return; 
+    this.calendarEvents = this.calendarEvents.concat({
+      title:title, 
+      start:date 
+    })
+    this.dayRender('rerenderEvents');
+  
+
+  }
+
+ 
 
   ngOnInit(): void {
   }
-
-  calendarPlugins = [dayGridPlugin];
-
 }
